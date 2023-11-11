1808aa6fd7f51baba50ee7a028aef7be89e8cfdf
Aber <abh9204@gmail.com>
2023-11-11 19:16:42 +0530

    fix button

--- src/app/Components/Dropdown.tsx
+++ src/app/Components/Dropdown.tsx
@@ -37,7 +37,7 @@ const Dropdown = () => {
   return (
     <div className="dropdown">
       {user && (
-        <div className="profile bg-black p-2 rounded-full text-white" onClick={toggleDropdown}>
+        <div className="profile bg-black p-2 rounded-lg text-white" onClick={toggleDropdown}>
           <span>🧑‍🦱Profile</span>
         </div>
       )}
